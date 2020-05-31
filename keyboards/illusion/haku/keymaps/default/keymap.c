5940053d625e130a9edf6afab1f1a14403042f37
Brandon Lee <bel291@nyu.edu>
2020-05-30 20:31:08 -0400

    toggle backlighting off

--- keyboards/illusion/haku/keymaps/default/keymap.c
+++ keyboards/illusion/haku/keymaps/default/keymap.c
@@ -6,15 +6,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS, KC_DEL,
         KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,  KC_PGUP,
         KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  KC_UP, KC_PGDN,
-        KC_LCTRL,   KC_LALT,                                    KC_SPACE,                   MO(1),   KC_RCTRL, KC_LEFT, KC_DOWN, KC_RIGHT
+        KC_LCTRL,   KC_APP,   KC_LALT,                                    KC_SPACE,                   MO(1),   KC_RCTRL, KC_LEFT, KC_DOWN, KC_RIGHT
     ),
     
     [1] = LAYOUT (
         KC_GRV, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, RGB_TOG,
-        KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, TRNS,   KC_TRNS, KC_SLCK, KC_NLCK, KC_TRNS, RGB_MOD,
+        KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_SLCK, KC_NLCK, KC_TRNS, RGB_MOD,
         KC_TRNS,    KC_TRNS,    RGB_SPD,    RGB_SPI,    KC_TRNS,    RGB_M_G,    KC_TRNS,    KC_TRNS,    RGB_M_K,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,
         KC_LSFT,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    RGB_M_B,    KC_TRNS,    KC_TRNS,    RGB_VAD, RGB_VAI,  KC_TRNS, KC_TRNS,  RGB_HUI, KC_TRNS,
-        KC_TRNS,   KC_TRNS,                                    RESET,                   KC_TRNS,   KC_TRNS, RGB_SAD, RGB_HUD, RGB_SAI
+        KC_TRNS,    KC_TRNS,   KC_TRNS,                                    RESET,                   KC_TRNS,   KC_TRNS, RGB_SAD, RGB_HUD, RGB_SAI
     )
 
 };
